06c40340d2ded2f6a6d692f48a86fdde1ad5f59f
Jonatan Vahlberg <jonatan.ramses.vahlberg@gmail.com>
2018-11-23 16:09:00 +0100

    Added some functionality to enemies and lives This is not a representation of how i would of liked it to looked but  a quick and dirty version after grading i will remake this

--- pacMan/script.js
+++ pacMan/script.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const lives = document.querySelector('#lives');
 const score = document.querySelector('#score');
+var moveAll;
+var framesCounter = 0; 
 
 var player = {
     //Position
@@ -12,9 +14,18 @@ var player = {
     lives: 3,
     score: 0,
     invounrable: 5,
-    level: 0
+    level: 0,
+    xDirection: 'null',
+    yDirection: 'null',
 };
+function newEnemy(x,y,xDirection,yDirection,color){
+    this.x = x;
+    this.y = y;
+    this.xDirection = xDirection;
+    this.yDirection = yDirection;
+    this.color = color;
 
+}
 var enemies = new Array();
 var coins = new Array();
 var controller = {
@@ -23,13 +34,8 @@ var controller = {
     up: false,
     down: false,
     keyListener:function(e) {
-        if(coins.every(coin => coin.collected == true)){
-            player.level++;
-            renderMap();
-            coins = new Array();
-        }
+        
         var state = (e.type == 'keydown')? true : false;
-        console.log(e.keyCode);
         switch(e.keyCode){
 
             case 37: //Left
@@ -56,27 +62,51 @@ var controller = {
 };
 
 function move(){
-    console.log(controller.right);
     ctx.fillStyle = '#ffffff';
     ctx.fillRect(player.x*50,player.y*50,50,50);
     if(controller.left == true){
-        checkMovement(player.x-1,player.y)
+        checkMovement(player.x-1,player.y,player)
     }
     if(controller.right == true){
-        checkMovement(player.x+1,player.y)
+        checkMovement(player.x+1,player.y,player)
     }
     if(controller.up == true){
-        checkMovement(player.x,player.y-1)
+        checkMovement(player.x,player.y-1,player)
     }
     if(controller.down == true){
-        checkMovement(player.x,player.y+1)
+        checkMovement(player.x,player.y+1,player)
     }
-    console.log(player.x,player.y)
     drawAll();
     ctx.fillStyle = '#89007B';
     ctx.fillRect(player.x*50,player.y*50,50,50);
     tileInteraction()
+    enemies.forEach(enemy =>{
+        ctx.fillStyle = enemy.color;
+    ctx.fillRect(enemy.x*50,enemy.y*50,50,50);
+    })
 }
+function moveEnemies(){
+    console.log(enemies[0]);
+    enemies.forEach(enemy => {
+        ctx.fillStyle = '#ffffff';
+        ctx.fillRect(enemy.x*50,enemy.y*50,50,50);
+        if(enemy.xDirection == '+'){    
+            checkMovement(enemy.x+1,enemy.y,enemy);
+        }else if(enemy.xDirection == '-'){
+            checkMovement(enemy.x-1,enemy.y,enemy)
+                
+        }
+        if(enemy.yDirection == '+'){
+            checkMovement(enemy.x,enemy.y+1,enemy);
+                
+        }else if(enemy.yDirection == '-'){
+            checkMovement(enemy.x,enemy.y-1,enemy);
+        }
+        ctx.fillStyle = enemy.color;
+        ctx.fillRect(enemy.x*50,enemy.y*50,50,50);
+    });
+}
+
 function createCoin (x,y,collected){
     this.x = x;
     this.y = y;
@@ -100,6 +130,16 @@ function drawAll(){
 }
 
 function tileInteraction(){
+    if(coins.every(coin => coin.collected == true)){
+            player.level++;
+            if(player.level >= maps.length){
+                alert('YOU WON CONGARTS');
+                location.reload();
+            }
+            coins = new Array();
+            renderMap();
+            window.clearInterval(moveAll);
+        }
     coins.forEach(coin => {
         if(player.x == coin.x && player.y == coin.y && coin.collected == false){
 
@@ -109,31 +149,55 @@ function tileInteraction(){
         }
         score.innerHTML = `SCORE: ${player.score}`;
     });
+    enemies.forEach(enemy =>{
+        if(player.x == enemy.x && player.y == enemy.y){
+            player.x = 0; player.y = 9;
+            player.lives--;
+            if(player.lives < 0){
+                alert('Game Over');
+                location.reload();
+            }
+            lives.innerHTML = `LIVES: ${player.lives}`;
+        }
+    })
 }
 
-function checkMovement(movedX,movedY){
-    console.log(maps[player.level][movedY][movedX])
+function checkMovement(movedX,movedY,obj){
     if(maps[player.level][movedY][movedX] == 1 ){
+        if(obj.xDirection == 'none'){
+            if(obj.yDirection == '+') obj.yDirection = '-';
+            else if(obj.yDirection == '-') obj.yDirection = '+';
+        }else if(obj.yDirection == 'none'){
+            if(obj.xDirection == '+') obj.xDirection = '-';
+            else if(obj.xDirection == '-') obj.xDirection = '+';
+        }
         return;
     }
-    console.log(movedX)
     if(movedX == (maps[player.level][movedY].length)){
-        player.x = 0;
-        player.y = 9;
+        obj.x = 0;
+        obj.y = 9;
+        
 
     }else if( movedX ==-1){
-        player.x = 15;
-        player.y = 8;
+        obj.x = 15;
+        obj.y = 8;
+        
     }
-    else if(!(movedX == player.x)){
-        player.x = movedX;
+    else if(!(movedX == obj.x)){
+        obj.x = movedX;
+        
     }else{
-        player.y = movedY
+        obj.y = movedY
+        
     }
+
 }
 renderMap();
 function renderMap(){
     var eachother = false;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    player.x = 0;
+    player.y = 9;
     for (let y = 0; y < maps[player.level][0].length; y++) {
         for (let x = 0; x < maps[player.level][y].length; x++) {
             
@@ -155,6 +219,17 @@ function renderMap(){
             
         }
         
+        
+    }
+    var enemy1 = new newEnemy(1,3,'+','none','green');
+    var enemy2 = new newEnemy(12,12,'none','+','red');
+
+    enemies.push(enemy1);
+    enemies.push(enemy2);
+    
+    if(player.level > 0){
+
+        moveAll = setInterval(move,150)
     }
 }
 
@@ -163,7 +238,19 @@ window.onload = function(){
 
     window.addEventListener('keydown',controller.keyListener);
     window.addEventListener('keyup',controller.keyListener);
-    setInterval(move,150);
+    moveAll = setInterval(() =>{
+            
+        move();
+        if(framesCounter > 450){
+            moveEnemies();
+            framesCounter = 0; 
+        }
+        else{
+
+            framesCounter += 150;
+        }
+
+    },150);
 
 };
 
